cb6e0c3d2b3a6d0d1fa2ce83f97fd069d4dcb4a8
yyf1986 <dn-yyf@163.com>
2017-08-27 16:14:08 +0800

    add

---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,26 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["whale/controllers:AgentInfoController"] = append(beego.GlobalControllerRouter["whale/controllers:AgentInfoController"],
+		beego.ControllerComments{
+			Method: "Post",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["whale/controllers:AgentInfoController"] = append(beego.GlobalControllerRouter["whale/controllers:AgentInfoController"],
+		beego.ControllerComments{
+			Method: "GetAll",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+}
